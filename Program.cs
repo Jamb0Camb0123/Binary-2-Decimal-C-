05c7580aeb01307567b10dab44b602b37e94487a
Jamie Campbell <137789482+Jamb0Camb0123@users.noreply.github.com>
2024-08-06 21:40:01 +0100

    Version 1
    Main Code Upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Bin2Dec
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Console Binary Converter in C#\r");
+            Console.WriteLine("------------------------\n");
+            bool repeater = true;
+            while (repeater)
+            {
+                string Bin = "";
+                Console.WriteLine("Insert a Binary Value: ");
+                Bin = Console.ReadLine();
+                try
+                {
+                    Console.WriteLine(Convert.ToInt32(Bin, 2).ToString() + "\n");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message + "\n");
+                }
+            }
+        }
+    }
+}
